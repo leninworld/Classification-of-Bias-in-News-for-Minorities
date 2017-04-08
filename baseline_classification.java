2dd70c6871c72140c9a54ed48be2de5eb5a3d8e1
Lenin Mookiah <lenin@Lenins-MBP.home>
2017-04-08 00:01:31 -0400

    First commit

---
+++ baseline_classification.java
@@ -0,0 +1,25 @@
+package p18;
+
+public class baseline_classification {
+	
+		// baseline classification
+		public baseline_classification(String inFile){
+			try {
+				
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				// TODO: handle exception
+			}
+		}
+		// main
+		public static void main(String[] args) throws Exception{
+		
+			
+			
+			
+			
+		}
+
+}
